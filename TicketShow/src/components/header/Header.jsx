6a2a180674355fbb3f2a9673a44f66fcc0b5df21
Harol <hchaverra26@gmail.com>
2024-04-30 12:56:46 -0500

    #2 Header: componente del header con buscador funcional que llama a una funcion que llama la api para hacer la consulta. agregado de carpetas: se agrego la carpta apis para hacer todas las consultas

---
+++ TicketShow/src/components/header/Header.jsx
@@ -0,0 +1,59 @@
+import './header.css'
+import {useState} from 'react'
+import {callSearch} from '../../Api/Search'
+
+let SearchEd = "";
+export const text = {
+  Text:"Ticket",
+  bold:"S",
+  textlag: "how"
+}
+ const links = {
+      link_c: "categories",
+      link_cI: "Ciudades",
+      link_cII: "home",
+      link_CIII: "about",
+      link_IV : "Contact"  
+  }
+
+export function HeaderGen() {
+
+  const [searched, SetSearched] = useState('');
+
+  const [activeModal,setModel] = useState("active");
+
+  const keyPress = (event)=>{
+    if(event.key === 'Enter'){
+        callSearch(SearchEd)
+      }
+  }
+  const handleChanged = (event) =>{
+    SetSearched(event.target.value);
+    SearchEd = event.target.value;
+  }
+    return (
+      <>
+      <header>
+        <div className="header-Page">
+            <div className="logo">
+                <a href="" className='logo'>{text.Text}<b className="RS-Logo">{text.bold}</b>{text.textlag}</a>
+            </div>
+            <div className='input-Search'>
+              <input
+              type="text" 
+              placeholder='Search your Events' 
+              id='told-BarSearch' 
+              onChange={handleChanged}
+              onKeyDown={keyPress}
+              />
+            </div>
+            <div className='link-Dynamic'>
+              
+                <a href="">{links.link_c}</a>
+                <a href="">{links.link_cI}</a>
+            </div>
+      </div>
+      </header>
+      </>
+    )
+  }
--- TicketShow/src/main.jsx
+++ TicketShow/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import {HeaderGen} from './components/header/Header.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+
+    <HeaderGen />
     <App />
   </React.StrictMode>,
 )
