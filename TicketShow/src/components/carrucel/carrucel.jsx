619197de650d3aefdad813da336d6013c4eb19a3
Harol <hchaverra26@gmail.com>
2024-05-08 15:28:17 -0500

    #6 - cambios en el carrucel

--- TicketShow/src/components/carrucel/carrucel.jsx
+++ TicketShow/src/components/carrucel/carrucel.jsx
@@ -6,38 +6,17 @@ import {useState} from 'react'
 
 export function CarrucelG() {
     return(
-    <>
     <div className='carrucel'>
         
         <div className='card-containers'>
 
-
                 <div className='cardCarrucel'>
                     <img src="" alt="" />
                      <div className='textContent'>
-                        <h1>Ths music is a relax <br></br>Andres Cepeda</h1>
+                        <h1>This music is a relax <br></br>Andres Cepeda</h1>
                         <p>12 agosto</p>
                     </div>
                 </div>
-
-                
-                <div className='cardCarrucel'>
-                    <img src="" alt="" />
-                     <div className='textContent'>
-                        <h1>Ths music is a relax <br></br>Andres Cepeda</h1>
-                        <p>12 agosto</p>
-                    </div>
-                </div>
-
-                <div className='cardCarrucel'>
-                    <img src="" alt="" />
-                     <div className='textContent'>
-                        <h1>Ths music is a relax <br></br>Andres Cepeda</h1>
-                        <p>12 agosto</p>
-                    </div>
-                </div>
-            
-
 
 
 
@@ -47,7 +26,6 @@ export function CarrucelG() {
 
 
     </div>
-    </>
     )
     
 }
